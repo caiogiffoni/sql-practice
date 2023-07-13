6d7ab430ec63c4f10355c4f7e6d297f09c28dd9b
Caio Giffoni <95258181+caiogiffoni@users.noreply.github.com>
2023-07-12 22:23:23 -0300

    Update variables.sql

--- variables.sql
+++ variables.sql
@@ -6,3 +6,36 @@ SET @div = CAST(@valor3 AS FLOAT) / CAST(@valor4 AS FLOAT)
 
 SELECT @soma, @subt, @mult, ROUND(@div, 2)
  
+DECLARE @produto VARCHAR(30) = 'Celular', @quantidade INT = 12, @preco FLOAT = 9.99, @faturamento FLOAT
+SET @faturamento = @preco * @quantidade
+
+SELECT @produto as prod, @quantidade, @preco, @faturamento
+
+
+DECLARE @nome VARCHAR(30) = 'André', @date DATETIME = '10/02/1998', @pets INT = 2
+PRINT'Meu nome é ' + @nome + ', nasci em ' + FORMAT(@date, 'dd/MM/yyyy') + ' e tenho ' + CAST(@pets AS VARCHAR(20)) + ' gatos'
+
+
+
+DECLARE @nome_loja VARCHAR(100)
+SET
+@nome_loja = ''
+SELECT
+	@nome_loja = @nome_loja + StoreName + ', '
+FROM
+	DimStore ds
+WHERE
+	status = 'Off'
+	AND CloseDate between '2008-01-01' and '2008-12-31'
+PRINT 'As lojas fechadas em 2008 foram ' + @nome_loja
+
+SELECT * from DimProduct dp JOIN DimProductSubcategory dps ON dps.ProductSubcategoryKey = dp.ProductSubcategoryKey WHERE ProductName LIKE '%Lumps%'
+
+SELECT * FROM DimProductSubcategory dps 
+
+
+
+DECLARE @id INT, @subcategoria VARCHAR(100)  = 'Radio'
+SET @id = (SELECT ProductSubcategoryKey  FROM DimProductSubcategory dps WHERE dps.ProductSubcategoryName = @subcategoria)
+
+SELECT * FROM DimProduct dp WHERE dp.ProductSubcategoryKey = ProductSubcategoryKey 
